4c7a65647bbaf019106715b4a2a2e90f18b7580f
Vennapusa Jithendra Reddy <106762673+Jithu2590@users.noreply.github.com>
2022-11-15 18:48:04 +0530

    Create WaterJug.py

---
+++ WaterJug.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+jug1=int(input("Enter capacity of j1 : "))
+jug2=int(input("Enter capcity of j2 : "))
+aim=int(input("Aim"))
+def deaf():
+	return False
+visited = defaultdict(deaf)
+def waterJugSolver(amt1, amt2):
+	if (amt1 == aim and amt2 == 0) or (amt2 == aim and amt1 == 0):
+		print('(',amt1, amt2,')')
+		return True
+	if visited[(amt1, amt2)] == False:
+		print('(',amt1, amt2,')',end="-->")
+		visited[(amt1, amt2)] = True
+		return (waterJugSolver(0, amt2) or
+				waterJugSolver(amt1, 0) or
+				waterJugSolver(jug1, amt2) or
+				waterJugSolver(amt1, jug2) or
+				waterJugSolver(amt1 + min(amt2, (jug1-amt1)),
+				amt2 - min(amt2, (jug1-amt1))) or
+				waterJugSolver(amt1 - min(amt1, (jug2-amt2)),
+				amt2 + min(amt1, (jug2-amt2))))
+	else:
+		return False
+print("Steps: ")
+waterJugSolver(0, 0)
